944086a1a77c4bb28d933fe04e586f3b5e984bfc
ChangeStrong <491337430@qq.com>
2018-06-15 17:25:20 +0800

    音频路径修改

--- Cartoon/app/src/main/java/com/example/luoluo/cartoon/UI/Login/PhoneLoginActivity.java
+++ Cartoon/app/src/main/java/com/example/luoluo/cartoon/UI/Login/PhoneLoginActivity.java
@@ -1,16 +1,29 @@
 package com.example.luoluo.cartoon.UI.Login;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.luoluo.cartoon.MainActivity;
 import com.example.luoluo.cartoon.R;
 
 public class PhoneLoginActivity extends Activity {
+    private Button loginbutton ;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_phone);
+        loginbutton = findViewById(R.id.loginButton);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PhoneLoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 
     @Override
